dc355e54c8cc91a159361eb429a5055be8d694b3
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2021-10-18 17:18:41 +0200

    Add ProductDto and IProductService

---
+++ Shared/Products/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Project.Shared.Products;
+
+public static class ProductDto
+{
+    public class Index
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
