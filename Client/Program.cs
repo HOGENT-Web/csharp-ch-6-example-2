e562ce237814f729e210dbcfab474047772572fa
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2022-10-17 17:50:51 +0200

    FakeAuthorization Infrastructure

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Project.Client;
+using Project.Client.Shared;
 using Project.Shared.Products;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,5 +11,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, FakeProductService>();
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddSingleton<AuthenticationStateProvider, FakeAuthenticationProvider>();
+
 
 await builder.Build().RunAsync();
