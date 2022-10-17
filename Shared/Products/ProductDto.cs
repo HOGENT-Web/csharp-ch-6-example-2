0d2302055766454b62d45030d7f20748d1d638fa
Benjamin Vertonghen <benjamin.vertonghen@append.be>
2022-10-17 12:46:29 +0200

    Add ProductDto.Detail

--- Shared/Products/ProductDto.cs
+++ Shared/Products/ProductDto.cs
@@ -1,4 +1,4 @@
-﻿namespace Project.Shared.Products;
+namespace Project.Shared.Products;
 
 public static class ProductDto
 {
@@ -7,5 +7,10 @@ public static class ProductDto
         public int Id { get; set; }
         public string? Name { get; set; }
         public decimal Price { get; set; }
+    }
+    public class Detail : Index
+    {
+        public string? Description { get; set; }
+        public string? Image { get; set; }
     }
 }
