9e761689b8cb2d4ad66add2ae1594cca60b4963a
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2022-10-20 12:38:57 +0200

    FakeProductService - Fake Details & Stop Overfetching

--- Shared/Products/FakeProductService.cs
+++ Shared/Products/FakeProductService.cs
@@ -7,21 +7,28 @@ namespace Project.Shared.Products;
 
 public class FakeProductService : IProductService
 {
-    private readonly List<ProductDto.Index> _products = new();
+    private readonly List<ProductDto.Detail> _products = new();
     public FakeProductService()
     {
         var productIds = 0;
-        var productFaker = new Faker<ProductDto.Index>("nl")
+        var productFaker = new Faker<ProductDto.Detail>("nl")
         .UseSeed(1337) // Always return the same products
         .RuleFor(x => x.Id, _ => ++productIds)
         .RuleFor(x => x.Name, f => f.Commerce.ProductName())
+        .RuleFor(x => x.Description, f => f.Commerce.ProductDescription())
+        .RuleFor(x => x.Image, f => f.Internet.Avatar())
         .RuleFor(x => x.Price, f => f.Random.Decimal(0, 250));
         _products = productFaker.Generate(25);
     }
 
     public Task<IEnumerable<ProductDto.Index>> GetIndexAsync()
     {
-        return Task.FromResult(_products.AsEnumerable());
+        return Task.FromResult(_products.Select(x => new ProductDto.Index
+        {
+            Id = x.Id,
+            Name = x.Name,
+            Price = x.Price
+        }));
     }
 
     public Task<ProductDto.Detail> GetDetailAsync(int productId)
